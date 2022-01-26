bd39a04202b8063f67a5a5b0d03d01e48977f688
Daniela Miñon <danielamactriz@gmail.com>
2022-01-25 18:26:55 -0600

    Versión uno de mi proyecto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-import validator from './validator.js';
+ import validator from './validator.js';
+document.getElementById("numeroT").addEventListener("click", validarTarjeta)
 
-console.log(validator);
+function validarTarjeta(){
+    let numberCard= document.getElementById("casilla1").value;
+//console.log("validarTarjeta");//
+validator.isValid(numberCard);
+let isValid=validator.isValid(numberCard);
+respuestaUsuario(isValid)
+validator.maskify(numberCard);
+let arrayCard= validator.maskify(numberCard);
+console.log(arrayCard);
+document.getElementById("casilla1").value= arrayCard;
+}
+    
+    
+function respuestaUsuario(isValid){
+    let respuestaUsuario=document.getElementById("resultado");
+    
+    if (isValid==true){
+    respuestaUsuario.innerHTML="Tu tarjeta es válida"
+    respuestaUsuario.classList.add("caja1");
+    respuestaUsuario.classList.remove("caja2");
+    
+
+   }
+
+    else{
+    respuestaUsuario.innerHTML= "Tu tarjeta es inválida"
+    respuestaUsuario.classList.add("caja2");
+    respuestaUsuario.classList.remove("caja1");
+    
+    }
+
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,79 @@
 const validator = {
-  // ...
+ isValid(numberCard){
+     if(numberCard== ""){
+        return false;
+     }
+     
+     let array1=numberCard.split("");
+     let reversaArray= array1.reverse();
+     for(let i= 1; i < reversaArray.length; i+=2) {
+        
+                reversaArray[i];
+                
+                let multipicacion= reversaArray[i];
+                let resultadoMultiplicacion= multipicacion *2; 
+                
+                reversaArray[i]= resultadoMultiplicacion;
+            
+            if(resultadoMultiplicacion>9){
+                        let sumarDosDigitos= sumarDigitos(resultadoMultiplicacion);
+                        
+                        reversaArray[i]= sumarDosDigitos;
+                        
+                    
+                }
+
+        }  
+        let numeroTotal= 0;
+        for(let i= 0; i < reversaArray.length; i+=1) {
+        
+            reversaArray[i]= parseInt(reversaArray[i]);
+
+              numeroTotal=numeroTotal+reversaArray[i];
+              
+           
+            
+        }
+
+        let moduloFinal= numeroTotal%10;
+        if(moduloFinal==0){
+            return true;
+        }
+        else{
+            return  false;
+        }
+        function sumarDigitos(numerosDosDigitos){
+            
+            let valorString= numerosDosDigitos.toString();
+            
+            let primerNumero= parseInt(valorString[0]);
+            let segundoNumero=parseInt(valorString[1]);
+            
+            let sumarDosDigitos= primerNumero + segundoNumero;
+
+            return sumarDosDigitos
+            
+            
+            
+            
+        }
+         
+},
+    
+ maskify(numberCard){
+ let arrayCard=numberCard.split("");
+ 
+ for(let i=0; i<arrayCard.length -4; i++){
+arrayCard[i]="#";
+
+
+ }
+ 
+ return arrayCard.join("")
+
+}  
+
+  
 };
 
 export default validator;
